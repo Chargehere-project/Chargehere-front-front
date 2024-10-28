8ab25bfb15d66e0fd7df6224d891876594707a13
Malbongs <qothgus13@naver.com>
2024-10-28 18:49:57 +0900

    Feat :  해

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Header />
-            <Component {...pageProps} />;
-            <Footer />
-        </>
-    );
+  const router = useRouter();
+  const path = router.pathname.startsWith("/mall");
+
+  return (
+    <>
+      {path ? "여기에 헤더를 추가하세요~~" : <Header />}
+      <Component {...pageProps} />;
+      <Footer />
+    </>
+  );
 }
