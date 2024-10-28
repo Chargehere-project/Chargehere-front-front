ff7f9b173a421984135d02e0d122de40fb6c58c7
Malbongs <qothgus13@naver.com>
2024-10-28 18:50:54 +0900

    Fix : 설명

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,8 @@ import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const path = router.pathname.startsWith("/mall");
+  const router = useRouter(); // router 사용
+  const path = router.pathname.startsWith("/mall"); // url 시작이 mall인지 확인 후에 있으면 true 없으면 false 반환
 
   return (
     <>
