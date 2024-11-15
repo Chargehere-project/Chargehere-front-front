c9b57801107dadd43746701f71d3d883348396ab
정주은 <jueun2812@naver.com>
2024-11-15 16:59:50 +0900

    qr버튼

--- src/components/mall/styled.ts
+++ src/components/mall/styled.ts
@@ -163,6 +163,8 @@ const MallStyled = styled.div`
         position: fixed;
         bottom: 20px;
         right: 20px;
+        width: 50px;
+        height: 50px;
         background-color: #333;
         color: #fff;
         border: none;
@@ -170,6 +172,25 @@ const MallStyled = styled.div`
         border-radius: 50%;
         cursor: pointer;
     }
+
+    .qrButton {
+        position: fixed;
+        bottom: 80px; /* 위로 올리기 버튼 위에 배치 */
+        right: 20px;
+        width: 50px;
+        height: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: black;
+        border: none;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        cursor: pointer;
+        }
+
+        :where(.css-dev-only-do-not-override-49qm).ant-btn-variant-solid {
+        background: black !important;
+        }
 
     /* 태블릿 (1024px 이하) */
     @media screen and (max-width: 1024px) {
