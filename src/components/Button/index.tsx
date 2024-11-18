6349c63778a5b3d906c33885ef0727eeeb0b05d5
정주은 <jueun2812@naver.com>
2024-11-18 15:19:24 +0900

    11.18

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,69 +1,23 @@
 import { ArrowUpOutlined, QrcodeOutlined } from '@ant-design/icons';
 import { Button, Modal } from 'antd';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import ButtonStyled from './styled';
-import { jwtDecode } from 'jwt-decode';
-import axios from 'axios';
 
 const Buttons = () => {
   const router = useRouter();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  // 로그인 여부를 확인하는 함수
-  const checkUserSession = async () => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      setIsLoading(false);
-      return false;
-    }
-
-    try {
-      const decoded: any = jwtDecode(token);
-      const userId = decoded.UserID;
-
-      const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/verify`,
-        { userId },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      if (response.data.result) {
-        setIsLoading(false);
-        return true;
-      } else {
-        localStorage.removeItem('token');
-        setIsLoading(false);
-        return false;
-      }
-    } catch (error) {
-      console.error('Error verifying user session', error);
-      localStorage.removeItem('token');
-      setIsLoading(false);
-      return false;
-    }
-  };
-
-  useEffect(() => {
-    checkUserSession();
-  }, [router.asPath]); // 라우터 경로가 변경될 때마다 체크
-
   // QR 버튼 클릭 핸들러
   const handleQRButtonClick = async () => {
-    setIsLoading(true);
-    const isAuthenticated = await checkUserSession();
-    
-    if (isAuthenticated) {
-      router.push('/charging');
-    } else {
-      setIsModalOpen(true);
+    const token = localStorage.getItem('token');
+    console.log(token,'token');
+    if (token) { 
+      router.push('/charging')      
+    } else { 
+      setIsModalOpen(true)
     }
-    setIsLoading(false);
   };
 
   // 모달 관련 함수
@@ -71,7 +25,6 @@ const Buttons = () => {
     setIsModalOpen(false);
     router.push('/mall/login');
   };
-
   const handleModalCancel = () => {
     setIsModalOpen(false);
   };
